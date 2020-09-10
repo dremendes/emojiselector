a73c90fbaee398f189f57321b06117a077164168
André Mendes <andre.mendesc@gmail.com>
2020-09-10 04:51:48 -0300

    Armazena quantidade de vezes que cada emoji foi clicado. Sort ainda não funciona e provavelmente precisa de coletagem de lixo

--- emojiSelector.c
+++ emojiSelector.c
@@ -8,9 +8,35 @@ typedef struct Emoji
     int index;
 };
 
-GtkClipboard *clipboard;
+GtkClipboard   *clipboard;
+GtkWidget      *window;
+GtkWidget      *emojisGrid;
+GtkWidget      *emoji;
+GtkSearchEntry *search_entry;
+GtkWidget      *label;
+
 struct Emoji emojis[260];
 int    count;
+char   arrayEmojis[260][28] = {"😀","😃","😄","😁","😆","😅","😂","🤣","😇","😉","😊","🙂","🙃","☺","😋","😌","😍","🥰","😘","😗","😙","😚","🤪","😜","😝","😛","🤑","😎","🤓","🧐","🤠","🥳","🤗","🤡","😏","😶","😐","😑","😒","🙄","🤨","🤔","🤫","🤭","🤥","😳","😞","😟","😠","😡","🤬","😔","😕","🙁","☹","😬","🥺","😣","😖","😫","😩","🥱","😤","😮","😱","😨","😰","😯","😦","😧","😢","😥","😪","🤤","😓","😭","🤩","😵","🥴","😲","🤯","🤐","😷","🤕","🤒","🤮","🤢","🤧","🥵","🥶","😴","💤","😈","👿","👹","👺","💩","👻","💀","☠","👽","🤖","🎃","😺","😸","😹","😻","😼","😽","🙀","😿","😾","👐🏿","🤲🏿","🙌🏿","👏🏿","🙏🏿","🤝","👍🏿","👎🏿","👊🏿","✊🏿","🤛🏿","🤜🏿","🤞🏿","✌🏿","🤘🏿","🤟🏿","👌🏿","🤏🏿","👈🏿","👉🏿","👆🏿","👇🏿","☝🏿","✋🏿","🤚🏿","🖐🏿","🖖🏿","👋🏿","🤙🏿","💪🏿","🦾","🖕🏿","✍🏿","🤳🏿","💅🏿","🦵🏿","🦿","🦶🏿","👄","🦷","👅","👂🏿","🦻🏿","👃🏿","👁","👀","🧠","🦴","👤","👥","🗣","👶🏿","👧🏿","🧒🏿","👦🏿","👩🏿","🧑🏿","👨🏿","👩🏿‍🦱","🧑🏿‍🦱","👨🏿‍🦱","👩🏿‍🦰","🧑🏿‍🦰","👨🏿‍🦰","👱🏿‍♀️","👱🏿","👱🏿‍♂️","👩🏿‍🦳","🧑🏿‍🦳","👨🏿‍🦳","👩🏿‍🦲","🧑🏿‍🦲","👨🏿‍🦲","🧔🏿","👵🏿","🧓🏿","👴🏿","👲🏿","👳🏿‍♀️","👳🏿","👳🏿‍♂️","🧕🏿","👼🏿","👸🏿","🤴🏿","👰🏿","🤵🏿‍♀️","🤵🏿","🤵🏿‍♂️","🙇🏿‍♀️","🙇🏿","🙇🏿‍♂️","💁🏿‍♀️","💁🏿","💁🏿‍♂️","🙅🏿‍♀️","🙅🏿","🙅🏿‍♂️","🙆🏿‍♀️","🙆🏿","🙆🏿‍♂️","🤷🏿‍♀️","🤷🏿","🤷🏿‍♂️","🙋🏿‍♀️","🙋🏿","🙋🏿‍♂️","🤦🏿‍♀️","🤦🏿","🤦🏿‍♂️","🙎🏿‍♀️","🙎🏿","🙎🏿‍♂️","🙍🏿‍♀️","🙍🏿","🙍🏿‍♂️","💇🏿‍♀️","💇🏿","💇🏿‍♂️","💆🏿‍♀️","💆🏿","💆🏿‍♂️","🤰🏿","🤱🏿","🚶🏿‍♀️","🚶🏿","🚶🏿‍♂️","🏃🏿‍♀️","🏃🏿","🏃🏿‍♂️","💃🏿","🕺🏿","👫🏿","👭🏿","👬🏿","🧑🏿‍🤝‍🧑🏿","👩‍❤️‍👨","👩‍❤️‍👩","👨‍❤️‍👨","👩‍❤️‍💋‍👨","👩‍❤️‍💋‍👩","👨‍❤️‍💋‍👨","🖤","❤","🍑","🍓","💥","💋"};
+
+static void
+action_click (GtkWidget *widget,
+             gpointer   data);
+
+static void
+filter_results (GtkSearchEntry *entry,
+                GtkLabel       *result_label)
+{
+  const char *textToFilter;
+
+  textToFilter = gtk_entry_get_text (GTK_ENTRY (entry));
+  g_message ("Filtering by: %s Found %i results", textToFilter, 0);
+  gtk_label_set_text (result_label,
+                      strcmp(textToFilter, "") 
+                      != 0 
+                        ? textToFilter 
+                        : "Digite para filtrar / Start Typing to filter");
+}
 
 static void
 copy_to_clipboard (GtkWidget *widget,
@@ -24,9 +50,12 @@ copy_to_clipboard (GtkWidget *widget,
 }
 
 static void
-increase_emoji_counter (struct Emoji *emojiArray, int index)
+increase_emoji_counter (gpointer  data)
 {
-  emojiArray[index].numTimesClicked++;
+  
+  emojis[(int) data].numTimesClicked++;
+  g_message ("Increased click counter for emoji %i", (gint *) data);
+  
 }
 
 static int
@@ -36,7 +65,42 @@ emoji_cmp (const void *e1, const void *e2)
   const struct Emoji *emoji2 = e2;
 
   return emoji1->numTimesClicked == emoji2->numTimesClicked ? 
-         0 : (emoji1->numTimesClicked < emoji2->numTimesClicked ? -1 : 0);
+         0 : (emoji1->numTimesClicked < emoji2->numTimesClicked ? 0 : -1);
+}
+
+static void
+sort_struct_emoji(struct Emoji *e[])
+{
+  qsort (e, count, sizeof (struct Emoji), emoji_cmp);
+  g_message ("Sorted emojis array, %s: %d, %s: %d", emojis[0].emojiCode , emojis[0].numTimesClicked, emojis[1].emojiCode, emojis[1].numTimesClicked);
+}
+
+static void
+redraw_buttons()
+{
+  GtkWidget      *emoji;
+
+  gtk_container_remove (GTK_CONTAINER (window), emojisGrid);
+  emojisGrid = gtk_grid_new ();
+  gtk_container_add (GTK_CONTAINER (window), emojisGrid);
+
+  for (int i = 0; i < 260; i++)
+  {
+      strncpy(arrayEmojis[i], emojis[i].emojiCode, sizeof(arrayEmojis[i]));
+      emoji = gtk_button_new_with_label (emojis[i].emojiCode);
+      g_signal_connect (emoji, "clicked", G_CALLBACK (action_click), i);
+      gtk_grid_attach (GTK_GRID (emojisGrid), emoji, i % 10, i / 10 + 2, 1, 1);
+  }  
+  count = sizeof (emojis) / sizeof (struct Emoji);
+
+  search_entry = gtk_search_entry_new ();
+  label = gtk_label_new ("Digite para filtrar / Start Typing to filter");
+  gtk_grid_attach (GTK_GRID (emojisGrid), GTK_WIDGET(search_entry), 0, 0, 11, 1);
+  gtk_grid_attach (GTK_GRID (emojisGrid), GTK_WIDGET(label), 0, 1, 11, 1);
+  g_signal_connect (search_entry, "search-changed",
+      G_CALLBACK (filter_results), label);
+
+  gtk_widget_show_all (window);
 }
 
 static void
@@ -44,36 +108,15 @@ action_click (GtkWidget *widget,
              gpointer   data)
 {
   copy_to_clipboard(widget, data);
-  increase_emoji_counter(emojis, (int) data);
-  qsort (emojis, count, sizeof (emojis), emoji_cmp);
-}
-
-static void
-filter_results (GtkSearchEntry *entry,
-                GtkLabel       *result_label)
-{
-  const char *textToFilter;
-  textToFilter = gtk_entry_get_text (GTK_ENTRY (entry));
-  g_message ("Filtering by: %s Found %i results", textToFilter, 0);
-  gtk_label_set_text (result_label,
-                      strcmp(textToFilter, "") 
-                      != 0 
-                        ? textToFilter 
-                        : "Digite para filtrar / Start Typing to filter");
+  increase_emoji_counter(data);
+  sort_struct_emoji(emojis);
+  redraw_buttons();
 }
 
 static void
 activate (GtkApplication* app,
           gpointer        user_data)
 {
-  GtkWidget      *window;
-  GtkWidget      *emojisGrid;
-  GtkWidget      *emoji;
-  GtkSearchEntry *search_entry;
-  GtkWidget      *label;
-
-  char  arrayEmojis[260][28] = {"😀","😃","😄","😁","😆","😅","😂","🤣","😇","😉","😊","🙂","🙃","☺","😋","😌","😍","🥰","😘","😗","😙","😚","🤪","😜","😝","😛","🤑","😎","🤓","🧐","🤠","🥳","🤗","🤡","😏","😶","😐","😑","😒","🙄","🤨","🤔","🤫","🤭","🤥","😳","😞","😟","😠","😡","🤬","😔","😕","🙁","☹","😬","🥺","😣","😖","😫","😩","🥱","😤","😮","😱","😨","😰","😯","😦","😧","😢","😥","😪","🤤","😓","😭","🤩","😵","🥴","😲","🤯","🤐","😷","🤕","🤒","🤮","🤢","🤧","🥵","🥶","😴","💤","😈","👿","👹","👺","💩","👻","💀","☠","👽","🤖","🎃","😺","😸","😹","😻","😼","😽","🙀","😿","😾","👐🏿","🤲🏿","🙌🏿","👏🏿","🙏🏿","🤝","👍🏿","👎🏿","👊🏿","✊🏿","🤛🏿","🤜🏿","🤞🏿","✌🏿","🤘🏿","🤟🏿","👌🏿","🤏🏿","👈🏿","👉🏿","👆🏿","👇🏿","☝🏿","✋🏿","🤚🏿","🖐🏿","🖖🏿","👋🏿","🤙🏿","💪🏿","🦾","🖕🏿","✍🏿","🤳🏿","💅🏿","🦵🏿","🦿","🦶🏿","👄","🦷","👅","👂🏿","🦻🏿","👃🏿","👁","👀","🧠","🦴","👤","👥","🗣","👶🏿","👧🏿","🧒🏿","👦🏿","👩🏿","🧑🏿","👨🏿","👩🏿‍🦱","🧑🏿‍🦱","👨🏿‍🦱","👩🏿‍🦰","🧑🏿‍🦰","👨🏿‍🦰","👱🏿‍♀️","👱🏿","👱🏿‍♂️","👩🏿‍🦳","🧑🏿‍🦳","👨🏿‍🦳","👩🏿‍🦲","🧑🏿‍🦲","👨🏿‍🦲","🧔🏿","👵🏿","🧓🏿","👴🏿","👲🏿","👳🏿‍♀️","👳🏿","👳🏿‍♂️","🧕🏿","👼🏿","👸🏿","🤴🏿","👰🏿","🤵🏿‍♀️","🤵🏿","🤵🏿‍♂️","🙇🏿‍♀️","🙇🏿","🙇🏿‍♂️","💁🏿‍♀️","💁🏿","💁🏿‍♂️","🙅🏿‍♀️","🙅🏿","🙅🏿‍♂️","🙆🏿‍♀️","🙆🏿","🙆🏿‍♂️","🤷🏿‍♀️","🤷🏿","🤷🏿‍♂️","🙋🏿‍♀️","🙋🏿","🙋🏿‍♂️","🤦🏿‍♀️","🤦🏿","🤦🏿‍♂️","🙎🏿‍♀️","🙎🏿","🙎🏿‍♂️","🙍🏿‍♀️","🙍🏿","🙍🏿‍♂️","💇🏿‍♀️","💇🏿","💇🏿‍♂️","💆🏿‍♀️","💆🏿","💆🏿‍♂️","🤰🏿","🤱🏿","🚶🏿‍♀️","🚶🏿","🚶🏿‍♂️","🏃🏿‍♀️","🏃🏿","🏃🏿‍♂️","💃🏿","🕺🏿","👫🏿","👭🏿","👬🏿","🧑🏿‍🤝‍🧑🏿","👩‍❤️‍👨","👩‍❤️‍👩","👨‍❤️‍👨","👩‍❤️‍💋‍👨","👩‍❤️‍💋‍👩","👨‍❤️‍💋‍👨","🖤","❤","🍑","🍓","💥","💋"};
-
   clipboard = gtk_clipboard_get(GDK_SELECTION_CLIPBOARD);
 
   window = gtk_application_window_new (app);
@@ -96,7 +139,7 @@ activate (GtkApplication* app,
       emojis[i].numTimesClicked = 0;
       emojis[i].index = i;
       emoji = gtk_button_new_with_label (emojis[i].emojiCode);
-      g_signal_connect (emoji, "clicked", G_CALLBACK (copy_to_clipboard), i);
+      g_signal_connect (emoji, "clicked", G_CALLBACK (action_click), i);
       gtk_grid_attach (GTK_GRID (emojisGrid), emoji, i % 10, i / 10 + 2, 1, 1);
   }  
 
